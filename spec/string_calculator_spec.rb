65e9812a072b88b897818540c3eaba576c112854
chetan1996dev <chetan1996dev@gmail.com>
2024-06-28 14:52:16 +0530

    Added test case for passing an empty string or numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,12 @@ RSpec.describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
   describe '#add' do
+    it 'returns 0 for an empty string' do
+      expect(calculator.add("")).to eq(0)
+    end
+
+    it 'returns the number for a single number string' do
+      expect(calculator.add("1")).to eq(1)
+    end
   end
 end
