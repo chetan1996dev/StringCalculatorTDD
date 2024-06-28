f9fcac79d03c9d72d7547ef3f99b61214eb5815d
chetan1996dev <chetan1996dev@gmail.com>
2024-06-28 15:05:10 +0530

    Added case for multiple numbers comma seperated and string with no numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -12,5 +12,21 @@ RSpec.describe StringCalculator do
     it 'returns the number for a single number string' do
       expect(calculator.add("1")).to eq(1)
     end
+
+    it 'returns the sum of two numbers' do
+      expect(calculator.add("1,2")).to eq(3)
+    end
+
+    it 'returns the sum of multiple numbers' do
+      expect(calculator.add("1,2,3")).to eq(6)
+    end
+
+    it 'handles a large number of numbers' do
+      expect(calculator.add("1,2,3,4,5,6,7,8,9,10")).to eq(55)
+    end
+
+    it 'handles a string with no numbers' do
+      expect(calculator.add(",")).to eq(0)
+    end
   end
 end
