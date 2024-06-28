37023138169d18d2f259616b4d5ace1f5c2ce2a8
chetan1996dev <chetan1996dev@gmail.com>
2024-06-28 15:22:47 +0530

    Handled condition for single or multiple negative values

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,18 @@ class StringCalculator
   def add(input)
     return 0 if input.empty?
     
-    input.split(/[,|\n]/).map{|num| num.to_i }.reduce(0, :+)
+    numbers = input.split(/[,|\n]/).map(&:to_i)
+
+    validate_numbers(numbers)
+
+    numbers.reduce(0, :+)
+  end
+
+  private
+
+  # Method to validate numbers for negative values
+  def validate_numbers(numbers)
+    negatives = numbers.select { |num| num < 0 }
+    raise "negative numbers not allowed: #{ negatives.join(', ') }" if negatives.any?
   end
 end
