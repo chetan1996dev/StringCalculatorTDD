fa7ef373113ce484b08a12525ca4f3d5e99ea1db
chetan1996dev <chetan1996dev@gmail.com>
2024-06-28 15:58:14 +0530

    Added support to support different delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,7 +3,8 @@ class StringCalculator
   def add(input)
     return 0 if input.empty?
     
-    numbers = input.split(/[,|\n]/).map(&:to_i)
+    delimiter, numbers_string = extract_delimiter(input)
+    numbers = numbers_string.split(delimiter).map(&:to_i)
 
     validate_numbers(numbers)
 
@@ -15,6 +16,18 @@ class StringCalculator
   # Method to validate numbers for negative values
   def validate_numbers(numbers)
     negatives = numbers.select { |num| num < 0 }
-    raise "negative numbers not allowed: #{ negatives.join(', ') }" if negatives.any?
+    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+  end
+
+  # Method to extract delimiter and numbers string
+  def extract_delimiter(input)
+    if input.start_with?("//")
+      delimiter = input[2]
+      numbers_string = input[4..]
+    else
+      delimiter = /[,\n]/
+      numbers_string = input
+    end
+    [delimiter, numbers_string]
   end
 end
