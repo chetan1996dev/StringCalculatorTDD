7bf1624aef6111cbc0a4041f2116905cfc9653b8
chetan1996dev <chetan1996dev@gmail.com>
2024-06-28 17:03:58 +0530

    Refactored and cleanup the code

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,28 +3,32 @@ class StringCalculator
   def add(input)
     return 0 if input.empty?
     
-    delimiter, numbers = extract_delimiter_and_numbers(input)
+    delimiter = extract_delimiter(input)
+    numbers = extract_numbers(input, delimiter)
     validate_numbers(numbers)
 
-    numbers.reduce(0, :+)
+    numbers.sum
   end
 
   private
+
+  # Method to extract delimiter from input string
+  def extract_delimiter(input)
+    if input.start_with?("//")
+      input.match(%r{//(.+)\n})[1].strip # Extract custom delimiter specified after '//'
+    else
+      ",|\n" # Default delimiters like comma or newline
+    end
+  end
+
+  # Method to extract numbers from string
+  def extract_numbers(input, delimiter)
+    input.gsub(%r{//(.+)\n}, "").split(/[#{delimiter}]/).map(&:to_i) # Remove delimiter and split string into numbers
+  end
 
   # Method to validate numbers for negative values
   def validate_numbers(numbers)
     negatives = numbers.select { |num| num < 0 }
     raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
-  end
-
-  # Method to extract delimiter and numbers string
-  def extract_delimiter_and_numbers(input)
-    if input.start_with?("//")
-      delimiter = input.match(%r{//(.+)\n})[1].strip
-    else
-      delimiter =  ",|\n"
-    end
-    numbers_string = input.gsub(%r{//(.+)\n}, "").split(/[#{delimiter}]/).map(&:to_i)
-    [delimiter, numbers_string]
   end
 end
